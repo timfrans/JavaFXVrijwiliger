9ce5d93f8413d4defae41e745c105941a850da09
Tim François <timiwow@outlook.com>
2019-02-15 10:40:01 +0100

    startup done

--- untitled/src/model/GuessStudent.java
+++ untitled/src/model/GuessStudent.java
@@ -1,3 +1,5 @@
+package model;
+
 public class GuessStudent extends Quiz {
     private Answer answer;
 }
--- untitled/src/model/Student.java
+++ untitled/src/model/Student.java
@@ -1,3 +1,5 @@
+package model;
+
 public class Student {
     private String name;
     private String imgUrl;
